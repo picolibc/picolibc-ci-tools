25e36341be80b48588360c780807560f6a7a8317
Keith Packard <keithp@keithp.com>
2026-03-08 13:01:39 -0700

    Build arm-toolchain
    Signed-off-by: Keith Packard <keithp@keithp.com>

---
+++ scripts/build_arm_toolchain.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Run in arm-toolchain/arm-software/embedded/build
+#
+# Then run
+#  $ ninja llvm-toolchain
+#
+# To package
+#  $ ninja package-llvm-toolchain
+#
+HERE="$(dirname "$0")"
+TOP="$HERE/.."
+export CC=clang
+export CXX=clang++
+cmake "$TOP"/arm-toolchain/arm-software/embedded -GNinja -DFETCHCONTENT_QUIET=OFF || exit 1
+ninja llvm-toolchain || exit 1
+ninja package-llvm-toolchain || exit 1
+mv ATfE-23.0.0-pre-Linux-x86_64.tar.xz "$TOP"
